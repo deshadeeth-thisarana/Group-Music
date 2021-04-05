aa8bc5952e64d9da9af2c1984ee83074744c065e
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-04-05 10:22:28 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -3,10 +3,8 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 from helpers.filters import other_filters2
 
-MUSICBOT_IMG = "https://telegra.ph/file/423b90ffebdfa598875b0.jpg"
-
 PM_START_TEXT = """
-I am an Telegram Groups Music bot 🎶 created by \n[Đ€Ş卄ΔĐ€€Ť卄 Ť卄ĪŞΔŘคŇΔ](t.me/DeshadeethThisarana), I let you play music in your group's voice chat.
+I am an Telegram Groups Music bot [🎶](https://telegra.ph/file/423b90ffebdfa598875b0.jpg) created by \n[Đ€Ş卄ΔĐ€€Ť卄 Ť卄ĪŞΔŘคŇΔ](t.me/DeshadeethThisarana), I let you play music in your group's voice chat.
 
 The commands I currently support are:
 
@@ -22,8 +20,7 @@ The commands I currently support are:
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
     await message.reply_text(
-         MUSICBOT_IMG,
-         PM_START_TEXT,
+        PM_START_TEXT,
         reply_markup=InlineKeyboardMarkup(
                [[
                         InlineKeyboardButton(
